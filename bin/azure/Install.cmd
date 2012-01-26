19c99be6b7c2709349ae5f02488ed22910e84766
Kousuke Ebihara <ebihara@php.net>
2012-01-26 16:07:54 +0900

    added support for re-installation (refs #3422)

--- bin/azure/Install.cmd
+++ bin/azure/Install.cmd
@@ -2,6 +2,8 @@
 
 ECHO "Starting PHP Installation" >> log.txt
 
+path >> log.txt
+
 md "%~dp0appdata"
 cd "%~dp0appdata"
 
@@ -15,17 +17,31 @@ reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user sh
 copy "..\php_azure.dll" "%ProgramFiles(x86)%\php\v5.3\ext"
 copy "..\php_apc.dll" "%ProgramFiles(x86)%\php\v5.3\ext"
 
-ECHO extension=php_azure.dll >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
-ECHO extension=php_apc.dll >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
+find "extension=php_azure.dll" "%ProgramFiles(x86)%\php\v5.3\php.ini"
+IF ERRORLEVEL 1 (
+    ECHO "" >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
+    ECHO "extension=php_azure.dll" >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
+)
+
+find "extension=php_apc.dll" "%ProgramFiles(x86)%\php\v5.3\php.ini"
+IF ERRORLEVEL 1 (
+    ECHO "" >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
+    ECHO "extension=php_apc.dll" >> "%ProgramFiles(x86)%\php\v5.3\php.ini"
+)
 
 cd ".."
 
 icacls %RoleRoot%\approot /grant "Everyone":F /T
+%WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /-"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='RoleDeploymentID']" /commit:apphost
+%WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='RoleDeploymentID',value='%RoleDeploymentID%']" /commit:apphost
+%WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /-"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='PATH']" /commit:apphost
 %WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='PATH',value='%PATH%;%RoleRoot%\base\x86']" /commit:apphost
 %WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /-"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='AZURE_ROLE_ROOT']" /commit:apphost
 %WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='AZURE_ROLE_ROOT',value='%RoleRoot%']" /commit:apphost
 
 ECHO "Completed PHP Installation" >> log.txt
+
+"%ProgramFiles(x86)%\php\v5.3\php.exe" -m >> log.txt
 
 ECHO "Starting OpenPNE Installation" >> log.txt
 
@@ -35,7 +51,16 @@ copy "OpenPNE.yml" "../../config"
 cd "../../"
 
 "%ProgramFiles(x86)%\php\v5.3\php.exe" symfony opPlugin:sync >>bin/azure/log.txt 2>>bin/azure/err.txt
-"%ProgramFiles(x86)%\php\v5.3\php.exe" symfony doctrine:build --all --and-load --no-confirmation >>bin/azure/log.txt 2>>bin/azure/err.txt
-"%ProgramFiles(x86)%\php\v5.3\php.exe" symfony cc
+
+"%ProgramFiles(x86)%\php\v5.3\php.exe" bin/azure/checkDBExists.php
+IF ERRORLEVEL 1 (
+    ECHO "Starting Database Initialization" >> bin/azure/log.txt
+    "%ProgramFiles(x86)%\php\v5.3\php.exe" symfony doctrine:build --all --and-load --no-confirmation >>bin/azure/log.txt 2>>bin/azure/err.txt
+) ELSE (
+    ECHO "Rebuilding Model Classes" >> bin/azure/log.txt
+    "%ProgramFiles(x86)%\php\v5.3\php.exe" symfony doctrine:build --all-classes --no-confirmation >>bin/azure/log.txt 2>>bin/azure/err.txt
+)
+
+"%ProgramFiles(x86)%\php\v5.3\php.exe" symfony cc >>bin/azure/log.txt 2>>bin/azure/err.txt
 
 ECHO "Completed OpenPNE Installation" >> bin/azure/log.txt
---
+++ bin/azure/checkDBExists.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!function_exists('azure_getconfig')) {
+    echo 'Please install php_azure.dll before executing this script.'.PHP_EOL;
+
+    exit(1);
+}
+
+try {
+    $dsn = sprintf('sqlsrv:Server=%s; Database=%s;', azure_getconfig('OPENPNE_DB_HOST'), azure_getconfig('OPENPNE_DB_NAME'));
+    $pdo = new PDO($dsn, azure_getconfig('OPENPNE_DB_USER'), azure_getconfig('OPENPNE_DB_PASSWORD'));
+} catch (PDOException $e) {
+    echo 'Connecting to database was failed with the following error(s): '.PHP_EOL.$e->getMessage().PHP_EOL;
+
+    exit(1);
+}
+
+$stmt = $pdo->prepare('SELECT COUNT(*) FROM sysobjects WHERE xtype = ?');
+$stmt->execute(array('U'));
+$result = $stmt->fetchColumn();
+
+exit((0 < $result) ? 0 : 1);
