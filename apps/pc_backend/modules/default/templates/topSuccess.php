c204e248a5c2e1f97bf470f70b281944548020b7
kaoru nishizoe <nishizoe@tejimaya.com>
2014-01-27 16:18:18 +0900

    管理画面のお知らせに、pne.jp/information.htmlの内容を表示するように変更

--- apps/pc_backend/modules/default/templates/topSuccess.php
+++ apps/pc_backend/modules/default/templates/topSuccess.php
@@ -25,4 +25,4 @@ function getDashboard(str)
 <script type="text/javascript" src="http://update.openpne.jp/?callback=getVersion&version=<?php echo OPENPNE_VERSION ?>"></script>
 
 <div id="dashboard" style="display: none;"></div>
-<script type="text/javascript" src="http://www.openpne.jp/dashboard/json?callback=getDashboard"></script>
+<script type="text/javascript" src="http://pne.jp/info.php?callback=getDashboard"></script>
