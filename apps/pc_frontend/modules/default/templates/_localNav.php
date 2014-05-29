a9b579067e00cbf795259a802adca015725241ce
Kimura Youichi <kim.upsilon@bucyou.net>
2014-05-29 20:53:44 +0900

    community_id=2 以外のコミュニティでナビゲーションの「つうちょう」を非表示にする

--- apps/pc_frontend/modules/default/templates/_localNav.php
+++ apps/pc_frontend/modules/default/templates/_localNav.php
@@ -2,10 +2,11 @@
 <ul class="<?php echo $type; ?>">
 <?php foreach ($navs as $nav): ?>
 
+<?php // 表示中のコミュニティIDに応じて非表示にするナビゲーション項目 ?>
 <?php if ('community' === $type && '2' === $navId): ?>
-<?php if (in_array(op_url_to_id($nav->uri), array('_community_join', '_community_quit'))): ?>
-<?php continue ?>
-<?php endif ?>
+<?php if (in_array(op_url_to_id($nav->uri), array('_community_join', '_community_quit'))) continue; ?>
+<?php else: ?>
+<?php if (in_array(op_url_to_id($nav->uri), array('_tsucho_index'))) continue; ?>
 <?php endif ?>
 
 <?php if (isset($navId)): ?>
